8b0be7630fb1c41675b617c65daf22024390f301
mdsimmo <mdsimmo2+git@gmail.com>
2019-09-14 21:43:28 -0700

    Prob 69

---
+++ prob_0069/src/lib.rs
@@ -0,0 +1,42 @@
+/*
+Given a list of integers, return the largest product that can be made by multiplying any three integers.
+
+For example, if the list is [-10, -10, 5, 2], we should return 500, since that's -10 * -10 * 5.
+
+You can assume the list has at least three integers.
+
+Time to complete: 7 min
+*/
+
+pub fn largest_multiple(numbers: &Vec<i32>) -> i32 {
+    let mut largest = vec![0, 0, 0];
+    let _a = 10i32.abs();
+    for &i in numbers {
+        let smallest = largest.clone().iter().enumerate().min_by_key(|(_, x)| x);
+        match smallest {
+            None => {
+                largest.push(i);
+
+            },
+            Some((index, &value)) => {
+                if largest.len() < 3 {
+                    largest.push(i)
+                } else if value < i {
+                     largest[index] = i;
+                }
+            }
+        }
+    }
+    largest.iter().fold(1, |a, &b| a * b)
+}
+
+#[cfg(test)]
+mod tests {
+    use largest_multiple;
+
+    #[test]
+    fn test_given() {
+        assert_eq!(500, largest_multiple(&vec![-10, -10, 5, 2]));
+    }
+
+}
